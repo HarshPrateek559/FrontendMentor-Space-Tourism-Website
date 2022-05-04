9fa1118a5115428530235f1cf5953f60e72039ed
HarshPrateek559 <neutronspace@gmail.com>
2022-05-04 11:09:48 +0530

    Completed tablet layout

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,5 @@
 import React from "react";
-import background from "../starter-code/assets/home/background-home-desktop.jpg";
 const Home = () => {
-  let style = {
-    background: `url(${background})`,
-    backgroundSize: "cover",
-    height: "100vh",
-    width: "100vw",
-  };
   return <div className="Home"></div>;
 };
 

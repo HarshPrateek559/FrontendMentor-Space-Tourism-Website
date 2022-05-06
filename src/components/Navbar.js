2b1bc00367288ede8b28edfaf40909fb934ae10d
HarshPrateek559 <neutronspace@gmail.com>
2022-05-06 14:52:58 +0530

    Completed the Desktop layout of Destination page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,33 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import openImg from "../starter-code/assets/shared/icon-hamburger.svg";
 import closeImg from "../starter-code/assets/shared/icon-close.svg";
 import logo from "../starter-code/assets/shared/logo.svg";
 const Navbar = () => {
-    let ul ="";
-    let navbar = "";
-    useEffect(() => {
-        ul = document.querySelector(".navlist");
-        navbar = document.querySelector('.navbar')
-    },[])//JS runs much earlier than HTML therefore querySelector would return null if not read after loading JSX, which could be done with useEffect Hook
-    
-  
+  let ul = "";
+  let navbar = "";
+  useEffect(() => {
+    ul = document.querySelector(".navlist");
+    navbar = document.querySelector(".navbar");
+  }, []); //JS runs much earlier than HTML therefore querySelector would return null if not read after loading JSX, which could be done with useEffect Hook
+
+  let location = useLocation();
+
   const handleOpen = () => {
     // ul.style.display = "block";
     navbar.style.height = "100vh";
-    if(ul.classList.contains('remove')){
-        ul.classList.remove('remove')
+    if (ul.classList.contains("remove")) {
+      ul.classList.remove("remove");
     }
-    ul.classList.add('animate')
+    ul.classList.add("animate");
   };
 
   const handleClose = () => {
     // ul.style.display = "none";
-    if(ul.classList.contains('animate')){
-        ul.classList.remove('animate')
+    if (ul.classList.contains("animate")) {
+      ul.classList.remove("animate");
     }
-    ul.classList.add('remove');
+    ul.classList.add("remove");
     setTimeout(() => {
       navbar.style.height = "10vh";
     }, 1000);
@@ -46,16 +47,33 @@ const Navbar = () => {
             <img src={closeImg} alt="" />
           </button>
           <li>
-            <Link to="/">00 Home</Link>
+            <Link to="/" className={location.pathname === "/" ? "active" : ""}>
+              00 Home
+            </Link>
           </li>
           <li>
-            <Link to="/destination">01 Destination</Link>
+            <Link
+              to="/destination"
+              className={location.pathname === "/destination" ? "active" : ""}
+            >
+              01 Destination
+            </Link>
           </li>
           <li>
-            <Link to="/crew">02 Crew</Link>
+            <Link
+              to="/crew"
+              className={location.pathname === "/crew" ? "active" : ""}
+            >
+              02 Crew
+            </Link>
           </li>
           <li>
-            <Link to="/technology">03 Technology</Link>
+            <Link
+              to="/technology"
+              className={location.pathname === "/technology" ? "active" : ""}
+            >
+              03 Technology
+            </Link>
           </li>
         </ul>
       </div>
